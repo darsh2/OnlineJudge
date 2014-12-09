a8631360e6c6489723d9469e74948f276e98f8f6
darsh2 <darsh.d2@gmail.com>
2014-12-09 12:19:06 +0530

    Added required actions

--- app/controllers/ProblemsController.java
+++ app/controllers/ProblemsController.java
@@ -3,8 +3,11 @@ package controllers;
 import backend.LocalStorage;
 import backend.Status;
 import backend.SubmissionStatus;
+import models.Comment;
 import models.Problem;
 import models.Submission;
+import models.User;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.*;
@@ -17,13 +20,25 @@ import java.util.Date;
  * Created by Darshan on 11/28/2014.
  */
 public class ProblemsController extends Controller {
+    public static class UserComment {
+        public String commentbody;
+
+        public String validate() {
+            if (commentbody.length() > 0)
+                return null;
+            return "Comment must not be empty";
+        }
+    }
+
+    private static Form<UserComment> form = Form.form(UserComment.class);
+
     public static Result viewProblems() {
         return ok(problems.render(Problem.getAllProblems()));
     }
 
     public static Result viewSelectedProblem(long id) {
         String directoryName = Long.toString(id);
-        return ok(views.html.displayproblem.render(directoryName, id, ""));
+        return ok(views.html.displayproblem.render(form, directoryName, id, "", Comment.getAllComments(id)));
     }
 
     public static Result submitSolution(long id) {
@@ -41,6 +56,20 @@ public class ProblemsController extends Controller {
             Problem.update(id);
         }
 
-        return ok(views.html.displayproblem.render(Long.toString(id), id, status.toString()));
+        return ok(views.html.displayproblem.render(form, Long.toString(id), id, status.toString(), Comment.getAllComments(id)));
+    }
+
+    public static Result addComment(long problemID) {
+        Form<UserComment> filledForm = form.bindFromRequest();
+        if (filledForm.hasErrors())
+            return badRequest(views.html.displayproblem.render(filledForm, Long.toString(problemID), problemID, "", Comment.getAllComments(problemID)));
+
+        Comment.addComment(session().get("handle"), problemID, filledForm.get().commentbody);
+        return redirect(controllers.routes.ProblemsController.viewSelectedProblem(problemID));
+    }
+
+    public static Result deleteComment(long problemID, String commentID) {
+        Comment.deleteComment(commentID);
+        return redirect(controllers.routes.ProblemsController.viewSelectedProblem(problemID));
     }
 }
